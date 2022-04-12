92c326a922c5276d3d189f855016f809ab630373
Nick Lu <tlu32@jhu.edu>
2022-04-11 21:01:45 -0400

    First commit

---
+++ src/main/java/hw7/hashing/ChainingHashMap.java
@@ -0,0 +1,46 @@
+package hw7.hashing;
+
+import hw7.Map;
+import java.util.Iterator;
+
+public class ChainingHashMap<K, V> implements Map<K, V> {
+  @Override
+  public void insert(K k, V v) throws IllegalArgumentException {
+    // TODO Implement Me!
+  }
+
+  @Override
+  public V remove(K k) throws IllegalArgumentException {
+    // TODO Implement Me!
+    return null;
+  }
+
+  @Override
+  public void put(K k, V v) throws IllegalArgumentException {
+    // TODO Implement Me!
+  }
+
+  @Override
+  public V get(K k) throws IllegalArgumentException {
+    // TODO Implement Me!
+    return null;
+  }
+
+  @Override
+  public boolean has(K k) {
+    // TODO Implement Me!
+    return false;
+  }
+
+  @Override
+  public int size() {
+    // TODO Implement Me!
+    return 0;
+  }
+
+  @Override
+  public Iterator<K> iterator() {
+    // TODO Implement Me!
+    return null;
+  }
+}
